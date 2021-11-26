324ea8d207c9661913d83c07a74fc89eb409eaa4
Yauheni Khvashcheuski <evgenijhq@gmail.com>
2021-11-26 17:18:56 -0500

    03. Working on modules.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.css';
+import * as menuPizza from './menu_modules/menu_pizza';
+import * as menuBeverages from './menu_modules/menu_beverages'
+import * as menuPasta from './menu_modules/menu_pasta';
+
+
+(function() {
+    const elMenuLink = document.getElementById('menuLink');
+    const elOrderBtn = document.getElementById('orderBtn');
+
+
+
+})();
